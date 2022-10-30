70c7565637bb401a7ca9530719040c111376aad3
aminmola <m.aminmolaeiarpanahi@gmail.com>
2022-10-30 16:21:28 +0330

    add the number of posts to api Image Search

--- main.py
+++ main.py
@@ -6,15 +6,19 @@ import requests
 import numpy as np
 
 app = FastAPI()
+
+
 # sp = Compair()
 
 
 @app.post("/")
-async def upload_file(file: UploadFile = File(...)):
+async def upload_file(postid_count: int = 5, file: UploadFile = File(...)):
+    # params = {"postid_count": n }
+
     a = requests.request("POST", "http://192.168.110.45:4050/", headers={}, data={},
-                           files=[('file', (file.filename, file.file, 'image/jpeg'))])
+                         files=[('file', (file.filename, file.file, 'image/jpeg'))])
     vec = np.array(a.json(), dtype=np.float32)
-    my_dict = {"similar_posts": similar_postid(vec)}
+    my_dict = {"similar_posts": similar_postid(vec, postid_count)}
     return my_dict
 
 
