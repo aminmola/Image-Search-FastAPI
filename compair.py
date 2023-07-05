e9b92ed5c52f4101f744a44e6ab7f341a03b917a
aminmola <m.aminmolaeiarpanahi@gmail.com>
2023-07-05 12:17:23 +0330

    not stable version

--- compair.py
+++ compair.py
@@ -1,12 +1,8 @@
 import numpy as np
-import pandas as pd
 from utils.mongo import Mongo
 import time
 import threading
 from sklearn.neighbors import NearestNeighbors
-
-vectors = {}
-knn = NearestNeighbors(n_neighbors=50, metric="cosine")
 
 
 class FeatureModel(Mongo):
@@ -18,50 +14,52 @@ class FeatureModel(Mongo):
 features_model = FeatureModel()
 
 
-def maximum(vecs: dict):
-    if vecs == {}:
-        mx = 8000000
-    else:
-        mx = max(vecs.keys())
-    return mx
+# def maximum(vecs: dict):
+#     if vecs == {}:
+#         mx = 10680000
+#     else:
+#         mx = max(vecs.keys())
+#     return mx
 
 
-def task():
-    m = maximum(vectors)
-    query = {"PostId": {'$gt': m}}
-    feature_docs = features_model.collection.find(query)
-    for vec in feature_docs:
-        vectors[vec['PostId']] = np.array(vec['Feature'], dtype=np.float32)
-    global v
-    v = np.array(list(vectors.values())).T
-    global knn
-    knn.fit(v.T)
-    global keys
-    keys = list(vectors.keys())
+class FindSimilar():
+    def __init__(self, ):
+        self.keys = []
+        self.knn = NearestNeighbors(n_neighbors=50, metric="cosine")
+        self.thread = threading.Thread(target=self.schedule)
+        self.thread.start()
+        self.vectors = {}
 
+    def schedule(self):
+        while 1:
+            self.update()
+            time.sleep(72000)
 
-def schedule():
-    while 1:
-        task()
-        time.sleep(7200)
-
-
-thread = threading.Thread(target=schedule)
-thread.start()
-
-
-def similar_postid(vec, k=5):
-    return [keys[i] for i in knn.kneighbors(np.expand_dims(vec, 0))[1][0][:k]]
-
-
-def similar_postid_exact(vec, k=50):
-    a = knn.kneighbors(np.expand_dims(vec, 0))
-    exact_similar_postid = []
-    for m in range(k):
-        if a[0][0][m] < 0.01:
-            exact_similar_postid.append(keys[a[1][0][m]])
+    def update(self):
+        if self.keys:
+            max_postId = max(self.keys)
         else:
-            break
-    return {"exact_similar_postid": exact_similar_postid,
-            "rest_of_similar_postid": [keys[i] for i in knn.kneighbors(np.expand_dims(vec, 0))[1][0][m:k]]
-            }
+            max_postId = 9000000
+        # m = maximum(vectors)
+        query = {"PostId": {'$gt': max_postId}}
+        feature_docs = features_model.collection.find(query)
+        for vec in feature_docs:
+            self.vectors[vec['PostId']] = np.array(vec['Feature'], dtype=np.float32)
+        v = np.array(list(self.vectors.values())).T
+        self.knn.fit(v.T)
+        self.keys = list(self.vectors.keys())
+
+    def similar_postid(self, vec, k=5):
+        return [self.keys[i] for i in self.knn.kneighbors(np.expand_dims(vec, 0))[1][0][:k]]
+
+    def similar_postid_exact(self, vec, k=50):
+        a = self.knn.kneighbors(np.expand_dims(vec, 0))
+        exact_similar_postid = []
+        for m in range(k):
+            if a[0][0][m] < 0.01:
+                exact_similar_postid.append(self.keys[a[1][0][m]])
+            else:
+                break
+        return {"exact_similar_postid": exact_similar_postid,
+                "rest_of_similar_postid": [self.keys[i] for i in self.knn.kneighbors(np.expand_dims(vec, 0))[1][0][m:k]]
+                }
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from compair import similar_postid, similar_postid_exact
+import compair
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
 from starlette.responses import FileResponse
@@ -6,6 +6,7 @@ import requests
 import numpy as np
 
 app = FastAPI()
+find_similar = compair.FindSimilar()
 
 
 @app.post("/")
@@ -13,7 +14,7 @@ async def upload_file(file: UploadFile = File(...), postid_count: int = 5):
     a = requests.request("POST", "http://192.168.110.45:4050/", headers={}, data={},
                          files=[('file', (file.filename, file.file, 'image/jpeg'))])
     vec = np.array(a.json(), dtype=np.float32)
-    my_dict = {"similar_posts": similar_postid(vec, postid_count)}
+    my_dict = {"similar_posts": find_similar.similar_postid(vec, postid_count)}
     return my_dict
 
 
@@ -22,7 +23,7 @@ async def upload_file(file: UploadFile = File(...), postid_count: int = 5):
     a = requests.request("POST", "http://192.168.110.45:4050/", headers={}, data={},
                          files=[('file', (file.filename, file.file, 'image/jpeg'))])
     vec = np.array(a.json(), dtype=np.float32)
-    my_dict = {"similar_posts": similar_postid_exact(vec, postid_count)}
+    my_dict = {"similar_posts": find_similar.similar_postid_exact(vec, postid_count)}
     return my_dict
 
 
